dd12a43934084c019cc411396da5449483fc2306
Diego Alejandro Torres Fuerte <diego-torres@users.noreply.github.com>
2025-08-15 18:51:36 -0600

    fix: adjust sshd session factory imports

--- src/main/java/io/redhat/na/ssp/tasktally/github/ssh/TaskTallySshdSessionFactory.java
+++ src/main/java/io/redhat/na/ssp/tasktally/github/ssh/TaskTallySshdSessionFactory.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 import java.security.KeyPair;
 import java.util.Collections;
 
+import org.apache.sshd.client.config.keys.KeyIdentityProvider;
+import org.apache.sshd.client.keyverifier.KnownHostsServerKeyVerifier;
 import org.apache.sshd.common.config.keys.FilePasswordProvider;
 import org.apache.sshd.common.util.security.SecurityUtils;
-import org.apache.sshd.client.auth.pubkey.KeyIdentityProvider;
-import org.eclipse.jgit.transport.sshd.KnownHostsServerKeyVerifier;
 import org.eclipse.jgit.transport.sshd.SshdSessionFactory;
 import org.eclipse.jgit.transport.sshd.SshdSessionFactoryBuilder;
 
@@ -25,7 +25,9 @@ public final class TaskTallySshdSessionFactory {
         passphrase == null
             ? FilePasswordProvider.EMPTY
             : (session, resourceKey, retryIndex) -> new String(passphrase);
-    KeyPair kp = SecurityUtils.loadKeyPairIdentity("key", new ByteArrayInputStream(privateKey), pwd);
+    KeyPair kp =
+        SecurityUtils.loadKeyPairIdentity(
+            null, "key", new ByteArrayInputStream(privateKey), pwd);
     KeyIdentityProvider provider = session -> Collections.singletonList(kp);
 
     SshdSessionFactoryBuilder builder = new SshdSessionFactoryBuilder();
