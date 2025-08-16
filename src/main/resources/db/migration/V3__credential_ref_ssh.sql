8d387f8f04c261355c05283c01a72e238268b29c
Diego Alejandro Torres Fuerte <diego-torres@users.noreply.github.com>
2025-08-16 09:55:05 -0600

    Consolidate flyway migrations into V1

--- src/main/resources/db/migration/V3__credential_ref_ssh.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE credential_refs ADD COLUMN known_hosts_ref TEXT;
-ALTER TABLE credential_refs ADD COLUMN passphrase_ref TEXT;
--- src/main/resources/db/migration/V3__ssh_secret_refs.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE credential_refs
-  ADD COLUMN IF NOT EXISTS known_hosts_ref TEXT,
-  ADD COLUMN IF NOT EXISTS passphrase_ref TEXT;
